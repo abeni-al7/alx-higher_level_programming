01dc0a0fcf864f384f2d2e414215dbdc4617034f
abeni-al7 <abenezeralebachew3@gmail.com>
2023-12-04 15:36:31 +0000

    Add tuples easily

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 1:
-            if len(tuple_b) != 0:
-                first = tuple_a[0] + tuple_b[0]
-                second = tuple_b[1] if len(tuple_b) >= 2 else 0
-            else:
-                first = tuple_a[0]
-                second = 0
-        else:
-            if len(tuple_b) != 0:
-                first = tuple_b[0]
-                second = tuple_b[1] if len(tuple_b) >= 2 else  0
-            else:
-                first = 0
-                second = 0
-    else:
-        if len(tuple_b) != 0:
-            first = tuple_a[0] + tuple_b[0]
-            second = tuple_a[1] + tuple_b[1] if len(tuple_b) >= 2 else tuple_a[1]
-        else:
-            first = tuple_a[0]
-            second = tuple_a[1]
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+
+    first = tuple_a[0] + tuple_b[0]
+    second = tuple_a[1] + tuple_b[1]
+
     return first, second
