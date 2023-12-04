d3b05d1b7b3c87d7a91c208f16e6b35e482b4100
abeni-al7 <abenezeralebachew3@gmail.com>
2023-12-04 15:10:06 +0000

    Check palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks a single list if it is a palindrome
+ * @head: pointer to pointer to the first element
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	unsigned int n = 0, i = 0;
+	listint_t *current = *head, *last = *head;
+	int *arr;
+
+	if ((*head) == NULL)
+		return (1);
+	while (last != NULL)
+	{
+		n++;
+		last = last->next;
+	}
+	arr = malloc(n * sizeof(int));
+	while (i < n)
+	{
+		arr[i] = current->n;
+		current = current->next;
+		i++;
+	}
+	for (i = 0, n -= 1; i < (n + 1) / 2; i++, n--)
+	{
+		if (arr[i] != arr[n])
+		{
+			free(arr);
+			return (0);
+		}
+	}
+	free(arr);
+	return (1);
+}
