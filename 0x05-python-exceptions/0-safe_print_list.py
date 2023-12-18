5d7798beb3bdfbf8aed2ea7dac514055f96ddbe7
abeni-al7 <abenezeralebachew3@gmail.com>
2023-12-18 15:00:05 +0000

    Print x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
         print()
         return x
-    except:
+    except IndexError:
         count = 0
         print()
         for item in my_list:
-            count = count + 1
+            count += 1
         return count
