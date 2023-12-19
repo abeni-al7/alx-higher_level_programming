59ea831849e4428683dd52bb0d2fb95dd75d3d8d
abeni-al7 <abenezeralebachew3@gmail.com>
2023-12-19 19:47:26 +0000

    Create a class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A simple square class with attributes.
+
+    Attributes:
+          size (int): the length of the side of the square
+    """
+    def __init__(self, size):
+        self.__size = size
