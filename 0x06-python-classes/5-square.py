3ba3615045f23ccf7108956e06d60a9b81064ac0
abeni-al7 <abenezeralebachew3@gmail.com>
2023-12-20 16:21:30 +0000

    Class square with another attribute

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,15 +7,14 @@ Defines a class with attributes.
 class Square:
     """
     A simple square class with attributes.
-     
+
     Attributes:
         size (int): the length of the side of the square
- 
-    Methods:
-        area: area of the square
-        my_print: prints the square object
-    """
 
+    Methods:
+        area(): area of the square
+        my_print(): prints the square object
+    """
 
     def __init__(self, size=0):
         if type(size) is not int:
@@ -30,18 +29,20 @@ class Square:
 
     @size.setter
     def size(self, value):
-        self.__size = value
-        if type(self.__size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if self.__size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         return self.__size * self.__size
+
     def my_print(self):
         if self.__size == 0:
             print()
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
-            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
