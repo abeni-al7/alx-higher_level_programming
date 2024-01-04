05e5dcc1fe2e5ae60ae404078f4a1215385a168f
abeni-al7 <abenezeralebachew3@gmail.com>
2024-01-04 17:25:14 +0000

    Class with attrib

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,10 +19,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """Getter for width"""
         return self.__width
 
     @width.setter
     def  width(self, value):
+        """Setter for width"""
         if isinstance(value) != int:
             raise TypeError('width must be an integer')
         if value < 0:
@@ -31,10 +33,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Getter for height"""
         return self.__height
     
     @height.setter
     def height(self, value):
+        """Setter for height"""
         if isinstance(value) != int:
             raise TypeError('height must be an integer')
         if value < 0:
