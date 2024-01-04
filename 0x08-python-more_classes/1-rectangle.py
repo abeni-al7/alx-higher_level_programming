9d91551e74530f8d4f78ddeaa9ff6d97085f395e
abeni-al7 <abenezeralebachew3@gmail.com>
2024-01-04 17:31:18 +0000

    Class with attrib

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,13 +10,15 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        """Initializer for object"""
+        """Initializer for object.
+
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
+
         self.__width = width
         self.__height = height
-        if isinstance(self.__width) != int:
-            raise TypeError('width must be an integer')
-        if self.__width < 0:
-            raise ValueError('width must be >= 0')
 
     @property
     def width(self):
@@ -25,8 +27,7 @@ class Rectangle:
 
     @width.setter
     def  width(self, value):
-        """Setter for width"""
-        if isinstance(value) != int:
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -40,7 +41,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Setter for height"""
-        if isinstance(value) != int:
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
