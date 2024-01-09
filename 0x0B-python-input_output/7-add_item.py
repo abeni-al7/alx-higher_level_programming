da759c7f80f00869d7f0c51f00a6bb43fc816a31
abeni-al7 <abenezeralebachew3@gmail.com>
2024-01-09 15:34:23 +0000

    File

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module for adding cl args to a list and saves it"""
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+my_list = []
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+save_to_json_file(my_list, "add_item.json")
