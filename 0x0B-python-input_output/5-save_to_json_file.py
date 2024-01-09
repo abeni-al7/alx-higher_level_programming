15dcad3c78e6621b637043c60f7b70bfbc0ece7a
abeni-al7 <abenezeralebachew3@gmail.com>
2024-01-09 14:53:19 +0000

    File

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """A function that serializes an object and stores it in a file"""
     with open(filename, "w", encoding="utf-8") as f:
-        json.dump(my_obj, f) 
+        json.dump(my_obj, f)
