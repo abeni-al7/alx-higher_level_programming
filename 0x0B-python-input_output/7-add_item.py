beab6a26600fdd87a1feaa06ce10aa106640b2e0
abeni-al7 <abenezeralebachew3@gmail.com>
2024-01-09 17:35:14 +0000

    File

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,9 +5,9 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if isinstance(load_from_json_file, list):
+try:
     my_list = load_from_json_file("add_item.json")
-else:
+except:
     my_list = []
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
