17f02ab6a8d17ce61a17febb2a85c9864ab5cef0
abeni-al7 <abenezeralebachew3@gmail.com>
2024-01-09 17:37:22 +0000

    File

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,14 +2,14 @@
 """A module for adding cl args to a list and saves it"""
 import json
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
-    my_list = load_from_json_file("add_item.json")
+    my_list = load("add_item.json")
 except:
     my_list = []
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
-save_to_json_file(my_list, "add_item.json")
+save(my_list, "add_item.json")
