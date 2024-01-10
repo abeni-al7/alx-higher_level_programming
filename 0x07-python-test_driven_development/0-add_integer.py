3bc6e2cc3a386b9be77bb7b19c16173b2c54a0b8
abeni-al7 <abenezeralebachew3@gmail.com>
2024-01-10 13:35:30 +0000

    Revert

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,7 +6,7 @@ A module that adds two integers.
 
 def add_integer(a, b=98):
     """A function that adds two integers"""
-    
+
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
     if not isinstance(b, (int, float)):
