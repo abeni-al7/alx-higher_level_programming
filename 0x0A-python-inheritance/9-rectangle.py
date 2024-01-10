c82e91d804d571ed3a270893316d1f4f73b23bd5
abeni-al7 <abenezeralebachew3@gmail.com>
2024-01-10 13:04:31 +0000

    Revert

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""A Rectangle class
-"""
-
-
+"""A Rectangle class"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """A Rectangle class"""
 
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int):
         """Initialize the rectangle class."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
-    def area(self):
+    def area(self) -> int:
         """Calculate area of rectangle"""
         return self.__width * self.__height
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "[{}] {}/{}".format(type(self).__name__,
-                                    self.__width, self.__height)
+                                   self.__width, self.__height)
+
