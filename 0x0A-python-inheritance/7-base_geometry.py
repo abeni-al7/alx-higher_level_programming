50725dcb9166cbd905a333dac2f3ec226b6f76c9
abeni-al7 <abenezeralebachew3@gmail.com>
2024-01-10 12:52:06 +0000

    Revert

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,6 +13,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """An integer validator function"""
         if type(value) != int:
-            raise TypeError('{} must be an integr'.format(name))
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
