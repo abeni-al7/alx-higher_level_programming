223082cf621b46a1ed0b2a1c0a28ef4df65a7060
Abenezer Endalew <abenezeralebachew3@gmail.com>
2024-02-15 03:16:05 +0300

    QUeries in sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- the number of shows linked to each
 SELECT tg.name AS genre, COUNT(tsg.genre_id) AS number_of_shows
 FROM tv_genres AS tg
-LEFT JOIN tv_show_genres AS tsg
+INNER JOIN tv_show_genres AS tsg
 ON tg.id = tsg.genre_id
 GROUP BY tg.name
 ORDER BY number_of_shows DESC;
