808abd2dacf031aaf4b44f5f1ec69debafec3919
Abenezer Endalew <abenezeralebachew3@gmail.com>
2024-02-15 03:10:49 +0300

    QUeries in sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,5 +4,5 @@ SELECT ts.title, tsg.genre_id
 FROM tv_shows AS ts
 LEFT JOIN tv_show_genres AS tsg
 ON ts.id = tsg.show_id
-WHERE ts.id = NULL
+WHERE tsg.genre_id IS NULL
 ORDER BY ts.title, tsg.genre_id;
