9a7ab0eec4ba28c53d4232081067d9b5581fcf13
Abenezer Endalew <abenezeralebachew3@gmail.com>
2024-02-15 03:32:16 +0300

    SQL fix

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Print the description of a table
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_oc_o' AND TABLE_NAME = 'first_table'
+SHOW COLUMNS FROM first_table;
