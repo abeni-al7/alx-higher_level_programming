04d79c84c7d5ef26f41cb47186027fc3f7bfd8ec
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-12 02:04:44 +0300

    HW js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 const args = process.argv;
+
 if (args.length <= 3) {
   console.log(0);
 } else {
   let max = Number(args[2]);
-  for (let i = 3; i < args.length; i++) {
-    const num = Number(args[i]);
-    args[i] = Number(args[i]);
-    if (num > max) {
-      max = num;
-    }
+  let secondMax = Number(args[3]);
+
+  if (secondMax > max) {
+    [max, secondMax] = [secondMax, max];
   }
-  let secondMax = Number(args[2]);
-  for (let i = 3; i < args.length; i++) {
+
+  for (let i = 4; i < args.length; i++) {
     const num = Number(args[i]);
-    if (num === max) {
-      continue;
-    }
-    if (num > secondMax) {
+    if (num > max) {
+      secondMax = max;
+      max = num;
+    } else if (num > secondMax) {
       secondMax = num;
     }
   }
+
   console.log(secondMax);
 }
