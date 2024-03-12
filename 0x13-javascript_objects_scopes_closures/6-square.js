7a96271c3944544a2b8c3a7147fcc796252ce523
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-12 23:33:18 +0300

    Empty class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
-const Rectangle = require('./5-square');
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super(size);
-    this.size = size;
+    super(size, size);
   }
 
   charPrint (c) {
     let line = '';
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.width; i++) {
       if (c !== undefined) {
         line += c;
       } else {
         line += 'X';
       }
     }
-    for (let j = 0; j < this.size; j++) {
+    for (let j = 0; j < this.height; j++) {
       console.log(line);
     }
   }
