d2b915cafaa3d711a28501925d12bf0c6bd9aed8
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-17 14:11:21 +0300

    Database query with mysqldb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
+"""A script that lists all states from a database"""
 import MySQLdb
 import sys
-"""A script that lists all states from a database"""
 
-args = sys.argv
-if len(args) > 3:
-    username = args[1]
-    password = args[2]
-    db_name = args[3]
-else:
-    username = ""
-    password = ""
-    db_name = ""
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) > 3:
+        username = args[1]
+        password = args[2]
+        db_name = args[3]
+    else:
+        username = "abeni_al7"
+        password = "Duolingo@mycrib"
+        db_name = "hbtn_0e_0_usa"
 
-connection = MySQLdb.connect(
-    host="localhost",
-    user=username,
-    passwd=password,
-    db=db_name,
-    port=3306
-)
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM states ORDER BY states.id")
-result = cursor.fetchall()
+    connection = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=db_name,
+        port=3306
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    result = cursor.fetchall()
 
-for row in result:
-    print(row)
+    for row in result:
+        print(row)
