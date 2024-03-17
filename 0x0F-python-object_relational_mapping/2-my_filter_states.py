10639822ceb53a3a9b77ec70d7af8fd290696eb1
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-18 01:07:50 +0300

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Searches data from a database"""
-import sys
+"""A script that provides a searched result from a database"""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    # Retrieve command line arguments
-    username, password, db_name, state_name = sys.argv[1:]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    searched = sys.argv[4]
 
-    # Connect to MySQL server
     connection = MySQLdb.connect(
         host="localhost",
         user=username,
@@ -15,21 +16,13 @@ if __name__ == "__main__":
         db=db_name,
         port=3306
     )
-
-    # Create cursor
     cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states\
+                   WHERE name = '{}'\
+                   ORDER BY id".format(searched))
+    result = cursor.fetchall()
 
-    # Prepare SQL query
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
-    cursor.execute(query, (state_name,))
-
-    # Fetch results
-    results = cursor.fetchall()
-
-    # Display results
-    for row in results:
+    for row in result:
         print(row)
-
-    # Close cursor and connection
     cursor.close()
     connection.close()
