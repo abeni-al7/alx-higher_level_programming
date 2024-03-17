1f8877ea0b02860d0b1a1f608f100a95fb737a03
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-17 14:46:13 +0300

    Database query with mysqldb

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 4:
         print("Usage: python script.py <username> <password> <database>")
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
