f6b1bb3d73858ded4cc121e973f94925a08e0c40
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-18 01:00:08 +0300

    ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,6 +2,7 @@
 """A model for State table"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -11,3 +12,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state")
