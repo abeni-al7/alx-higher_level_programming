df948c8e7891b0a6b6c14f689972e35fdc149490
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-29 00:24:34 +0300

    Display

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # This script displays the size of the body of the response
-# it gets from the URL it sends the request to
-# The URL is provided as an argument
-curl -sI $1 | grep -i Content-Length | awk {'print $2'};
+curl -sI $1 | grep -i Content-Length | awk {'print $2'}
