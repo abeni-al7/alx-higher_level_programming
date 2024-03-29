aabd0e8378f67d2db7c02ca77b97d4b756c05ac5
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-29 15:17:25 +0300

    Fetch resource

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,8 @@ import urllib.request
 
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-	data = response.read()
-	print('Body response:')
-	print('\t- type: {}'.format(type(data)))
-	print('\t- content: {}'.format(data))
-	print('\t- utf8 content: {}'.format(data.decode('utf-8')))
+    data = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(data)))
+    print('\t- content: {}'.format(data))
+    print('\t- utf8 content: {}'.format(data.decode('utf-8')))
