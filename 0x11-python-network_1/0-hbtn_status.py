d828b8909557f8918fa7dbb0d25764bd4931d881
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-29 15:31:13 +0300

    Get a header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@
 import urllib.request
 
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    data = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(data)))
-    print('\t- content: {}'.format(data))
-    print('\t- utf8 content: {}'.format(data.decode('utf-8')))
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        data = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))
+        print('\t- utf8 content: {}'.format(data.decode('utf-8')))
