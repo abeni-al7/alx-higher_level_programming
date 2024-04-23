42a66d95b40b4da66eef33c06e4b8496da6874ad
abeni-al7 <abenezeralebachew3@gmail.com>
2024-04-23 20:50:42 +0300

    Writing to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// This script accepts a file path and some string to write to
+// the file. It writes the  ontent of the string to the file
+// or the error object.
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile
+  (filePath, content, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
